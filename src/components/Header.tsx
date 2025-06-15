2d033c2723006c40b4fc4e1f1ea9bd6aceffec83
javier <espindolajavier2013@gmail.com>
2025-06-15 10:15:28 -0300

    `feat: Added El menú de hamburguesas y el menú móvil se vinculan al componente Encabezado`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { ItemHeaderProps } from "@/lib/definitions";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
-
   const itemsArr: ItemHeaderProps[] = [
     { title: "Home", url: "#" },
     { title: "Shop", url: "#" },
@@ -10,9 +11,38 @@ export default function Header() {
     { title: "Contact", url: "#" },
   ];
 
+  const [open, setOpen] = useState(false);
+
+
+
   return (
-    <header className="relative top-0 left-0 w-full">
+    <header className="absolute top-0 left-0 w-full z-50">
       <nav className="flex items-center px-4 py-2">
+        <div className="flex items-center justify-center mr-8">
+          <button type="button" onClick={() => 
+            setOpen(!open)}>
+            <Image
+              className={`object-contain ${open ? "hidden" : "block"} sm:hidden`}
+              src="/assents/images/icon-hamburger.svg"
+              alt="menu"
+              width={20}
+              height={20}
+              
+            />
+          </button>
+
+          <button type="button" onClick={() => 
+            setOpen(!open)}>
+            <Image
+              className={`object-contain ${open ? "block" : "hidden"} sm:hidden`}
+              src="/assents/images/icon-close.svg"
+              alt="menu-close"
+              width={20}
+              height={20}
+              
+            />
+          </button>
+
         <a className="flex items-center justify-center mr-8" href="#">
           <Image
             className="object-contain"
@@ -22,9 +52,18 @@ export default function Header() {
             height={50}
           />
         </a>
-        <ul className="flex gap-11 items-center">
+            </div>
+        
+        <ul className={`flex gap-11 items-center `}>
           {itemsArr.map((item: ItemHeaderProps, index) => (
-            <LinkHeader key={item.title+index+"header"} {...item} />
+            <LinkHeader key={item.title + index + "header"} {...item} />
+          ))}
+
+          {itemsArr.map((item: ItemHeaderProps, index) => (
+            <LinkHeaderMobile
+              key={item.title + index + "headerMobile"}
+              {...item} open={open} index={index}
+            />
           ))}
         </ul>
       </nav>
@@ -34,12 +73,27 @@ export default function Header() {
 
 export function LinkHeader({ title, url }: ItemHeaderProps) {
   return (
-    <li className="relative">
-      <a 
+    <li className="relative hidden sm:block ">
+      <a
         href={url}
         className="lowercase font-bold py-2 px-1 block transition-all duration-300 ease-in-out hover:cursor-pointer relative group"
       >
-        <span>{title}</span>
+        <span className="text-white">{title}</span>
+        {/* Línea de subrayado animada */}
+        <span className="absolute bottom-0 left-2 right-2 h-0.5 bg-current transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+      </a>
+    </li>
+  );
+}
+
+export function LinkHeaderMobile({ title, url, open = false , index}: {title: string, url: string, open?: boolean, index: number}) {
+  return (
+    <li className={`absolute  w-full h-full top-0  left-[${index*100}px] ${open ? "block" : "hidden"} items-center justify-center  sm:hidden`}>
+      <a
+        href={url}
+        className="lowercase font-bold py-2 px-1 block transition-all duration-300 ease-in-out hover:cursor-pointer relative group"
+      >
+        <span className="text-black">{title}</span>
         {/* Línea de subrayado animada */}
         <span className="absolute bottom-0 left-2 right-2 h-0.5 bg-current transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
       </a>
